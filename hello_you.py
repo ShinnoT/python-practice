86ac6f2a2cc0e23ef06577d9ea782fbb8bb88365
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-11-26 08:36:34 +0900

    asked user for input

---
+++ hello_you.py
@@ -0,0 +1,11 @@
+#ask user for name
+name = input("What is your name?: ")
+#ask user for age
+age = input("What is your age?: ")
+#ask user for city
+city = input("Which city do you live in?: ")
+#ask user what they enjoy
+hobby = input("What do you enjoy doing in your free time?: ")
+#create output text
+output =
+#print output to screen

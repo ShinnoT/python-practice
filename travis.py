ea04afe6afc8b1a7d5285218f89c0b36eaa93451
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-04 01:10:08 +0900

    allows users to delete themselves from the list if name is recognized by travis

--- travis.py
+++ travis.py
@@ -7,6 +7,22 @@ while True:
   name = input("Whats your name?: ").strip().lower()
 
   if name in known_users:
-    print("name recognized")
+    print("name recognized! hello {}".format(name))
+    remove = input("would you like to be removed from the system (y/n)?: ").strip().lower()
+
+    if remove == "y":
+      known_users.remove(name) #only deletes the first instance of name so if name occurs twice it wont work on the second one
+      #if you want to remove an element from the list using an index then do
+      #del known_users[0]
+      #OR del known_users[0:3]
+      #OR del known_users[0::2]
+      print("you are removed!")
+    else:
+      print("cool thanks for using the app")
+
   else:
     print("name NOT recognized")
+    add = input("would you live to be added (y/n)?: ").strip().lower()
+
+    if add == "y":
+

3904e5cee14cf02b32472718739daa1fd53bb66d
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-06 18:16:30 +0900

    pig latin generator logic complete

--- pig_latin_generator.py
+++ pig_latin_generator.py
@@ -7,3 +7,31 @@
 #stick words back together
 
 #output the final string
+
+
+sentence = input("please type a sentence: ").strip().lower()
+words = sentence.split() #will generate a list of words from the sentence
+
+new_words = []
+for word in words:
+  #code that converts each word into pig latin
+  if word[0] in "aeiou":
+    new_word = word + "yay"
+    new_words.append(new_word)
+  else:
+    vowel_index = 0
+    for letter in word:
+      if letter not in "aeiou":
+        vowel_index += 1
+      else:
+        break
+    print(vowel_index)
+    consonants = word[:vowel_index]
+    the_rest = word[vowel_index:]
+    new_word = the_rest + consonants + "ay"
+    new_words.append(new_word)
+
+print(new_words)
+
+new_sentence = " ".join(new_words) #joining new_words list with a space
+print(new_sentence)

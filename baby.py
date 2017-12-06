05d49575d52eae23ca642eb34f38e50056131495
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-06 16:20:09 +0900

    string formatting to  question so that the user interface is friendlier

--- baby.py
+++ baby.py
@@ -1,9 +1,9 @@
 from random import choice
 
 
-questions = ["why is the sky blue?", "how does the internet work?", "where are dinasours?"]
+questions = ["why is the sky blue", "how does the internet work", "where are dinasours"]
 question = choice(questions)
-answer = input(question).strip().lower()
+answer = input("{}?: ".format(question)).strip().lower()
 
 while answer != "just because":
   answer = input("why?: ").strip().lower()

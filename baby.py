a5b301c9c74e7559780c480b6b9d4a2d5fd7b44b
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-06 16:07:10 +0900

    while loop that simulates a baby asking questions complete

---
+++ baby.py
@@ -0,0 +1,9 @@
+from random import choice
+
+
+questions = ["why is the sky blue?", "how does the internet work?", "where are dinasours?"]
+question = choice(questions)
+answer = input(question).strip().lower()
+
+while answer != "just because":
+  answer = input("why?: ").strip().lower()

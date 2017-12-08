5180e73e9a4e51109f1cbcc165b5908dc0398c89
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-08 23:11:11 +0900

    board layout complete

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,3 +5,16 @@ board = [" " for i in range(9)]
   #for i in range(9)
     #board.append(" ")
 
+def print_board():
+  row1 = "|{}|{}|{}|".format(board[0], board[1], board[2])
+  row2 = "|{}|{}|{}|".format(board[3], board[4], board[5])
+  row3 = "|{}|{}|{}|".format(board[6], board[7], board[8])
+
+  print()
+  print(row1)
+  print(row2)
+  print(row3)
+  print()
+
+
+

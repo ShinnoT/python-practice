7c81dd28126ed6765b615ccc1ee8988e4be2da90
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-11 20:45:10 +0900

    3 instances of class, one with different attributes

--- coins.py
+++ coins.py
@@ -6,3 +6,17 @@ class Pound:
   diameter = 22.5 #milimeters
   thickness = 3.15 #milimeters
   heads = True
+
+
+coin1 = Pound() #new instance of Pound class
+#we can access attributes like this:
+coin1.color # will return "gold"
+
+coin2 = Pound()
+#changing attributes of instances
+coin2.color = "red"
+coin2.color # will now return "red"
+#but default color for all Poun instances still remain "gold"
+
+coin3 = Pound()
+coin3.color # will still return "gold"
